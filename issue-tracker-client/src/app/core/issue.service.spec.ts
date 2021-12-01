6b7f1175d3d2c06b89663b1520e941fa447092f0
tibor.moger <tibor.moger@smartx.eu>
2021-12-01 09:49:30 +0100

    angular testing

--- issue-tracker-client/src/app/core/issue.service.spec.ts
+++ issue-tracker-client/src/app/core/issue.service.spec.ts
@@ -1,16 +1,34 @@
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 
 import { IssueService } from './issue.service';
 
 describe('IssueService', () => {
+  let httpTestingController: HttpTestingController;
   let service: IssueService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    });
     service = TestBed.inject(IssueService);
+    httpTestingController = TestBed.inject(HttpTestingController)
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  describe('getIssues', () => {
+    it('should call httpClient get with /api/issues', async () => {
+      const issuesPromise = service.getIssues();
+
+      httpTestingController.expectOne('/api/issues').flush([]);
+
+      expect(await issuesPromise).toEqual([]);
+
+      httpTestingController.verify();
+    });
+
   });
 });
--- issue-tracker-client/src/app/issue-summary/issue-summary.component.spec.ts
+++ issue-tracker-client/src/app/issue-summary/issue-summary.component.spec.ts
@@ -1,4 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { RouterTestingModule } from '@angular/router/testing';
+import { IssueStatus } from '../core/issue';
+import { UserRole } from '../core/user';
+import { UserService } from '../core/user.service';
+import { first } from 'rxjs/operators';
 
 import { IssueSummaryComponent } from './issue-summary.component';
 
@@ -8,18 +14,45 @@ describe('IssueSummaryComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ IssueSummaryComponent ]
-    })
-    .compileComponents();
+      imports: [RouterTestingModule],
+      declarations: [IssueSummaryComponent],
+      providers: [
+        {
+          provide: UserService,
+          useValue: { isAdmin: true },
+        },
+      ],
+    }).compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(IssueSummaryComponent);
+
     component = fixture.componentInstance;
+    component.issue = {
+      title: 'Rossz projektor',
+      description: 'asd',
+      place: 'asd',
+      status: IssueStatus.Done,
+      user: {
+        name: 'Tibi',
+        role: UserRole.User,
+      },
+    };
+    component.showButtons = true;
+
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should emit editIssue when clicking on edit issue', () => {
+    component.editIssue.pipe(first()).subscribe(() => {
+      expect(true).toBe(true);
+    });
+    const editIssueButton = fixture.debugElement.query(By.css('#editIssue'));
+    editIssueButton.triggerEventHandler('click', null);
   });
 });
